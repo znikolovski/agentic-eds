7a2f7b1eac2af18734b2a54ef7dcba9d4da865cc
Zoran Nikolovski <nikolovs@adobe.com>
2026-03-04 12:10:15 +1100

    Update import path for showSlide function

--- scripts/ue.js
+++ scripts/ue.js
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-import { showSlide } from '../../blocks/carousel/carousel.js';
+import { showSlide } from '../blocks/carousel/carousel.js';
 import { moveInstrumentation } from './ue-utils.js';
 
 const setupObservers = () => {
